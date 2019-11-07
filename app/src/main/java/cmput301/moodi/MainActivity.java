b45ed08d859da4d36bf051db958efddab197462d
andrew-t-williams <andrew-t-williams@users.noreply.github.com>
2019-11-06 21:26:58 -0700

    Firebase authentification is working. I renamed classes to MainActivity (log in), CreateAcount, and HomeActivity. Users create an account with an email and password, firebase handles the authentification and storage of user information.

--- app/src/main/java/cmput301/moodi/MainActivity.java
+++ app/src/main/java/cmput301/moodi/MainActivity.java
@@ -2,127 +2,108 @@ package cmput301.moodi;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
-    // temp comment. delete this
+    // log in page objects
+    Button button_login, button_goto_signup;
+    EditText emailId, password;
 
-    // Variables that are pointed to login activity to retrieve data
-    Button LoginButton;
-    Button NewAccountButton;
-    EditText UserID;
-    EditText UserPassword;
-
-    // Initialization of firebase
-    FirebaseFirestore db;
-    String TAG = "Sample";
-
+    // firebase auth link
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
-        // OnClick listeners
-        configureLoginButton();
-        configureNewAccountButton();
+        // link to firebase authentication
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-    }
+        // create all references to the layout
+        emailId = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        button_login = findViewById(R.id.login_button);
+        button_goto_signup = findViewById(R.id.goto_signup);
 
-    private void configureLoginButton() {
-        // Pointing variables to the buttons and text
-        LoginButton = findViewById(R.id.Login_Button);
-        UserID = findViewById(R.id.UserID_text);
-        UserPassword = findViewById(R.id.Password_text);
-
-        // Adding an onClickListener to the login button awaiting a click.
-        LoginButton.setOnClickListener(new View.OnClickListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-
-                // Retrieving the user name and the password from the EditText fields.
-                final String userID = UserID.getText().toString();
-                final String password = UserPassword.getText().toString();
-
-                // We use a HashMap to store a key-value pair in firestore. Can you guess why? Because it's a No-SQL database.
-                HashMap<String, String> data = new HashMap<>();
-
-                // Access a Cloud Firestore instance from your Activity
-                db = FirebaseFirestore.getInstance();
-
-                // Get a top-level reference to the collection.
-                final CollectionReference collectionReference = db.collection("users");
-
-                // Checking if fields are empty
-                if (userID.length() > 0 && password.length() > 0) {
-
-                    // If there is some data in the EditText field, then we create a new key-value pair.
-                    data.put("password", password);
-
-                    // The set method sets a unique id for the document.
-                    collectionReference
-                            .document(userID)
-                            .set(data)
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    // These are a method which gets executed when the task is successful.
-                                    Log.d(TAG, "Data addition successful");
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    // This method gets executed if there is any problem.
-                                    Log.d(TAG, "Data addition failed" + e.toString());
-                                }
-                            });
-
-                    // Check for an existing ID
-                    // If correct change activity to Activity main else reprompt login
-
-                    // Setting the fields to null so the user can add a new city.
-                    UserID.setText("");
-                    UserPassword.setText("");
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if (mFirebaseUser != null) {
+                    Toast.makeText(MainActivity.this, "Log in Successful.", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(i);
+                } else if (mFirebaseUser == null){
+                    //Toast.makeText(MainActivity.this, "Unable to log in.", Toast.LENGTH_SHORT).show();
                 }
-            startActivity(new Intent(MainActivity.this, MoodsActivity.class ));
             }
-        });
+        };
 
-    }
 
-    // OnClick listener for "Create New Account" button
-    private void configureNewAccountButton() {
-        // Pointing variable to the button
-        NewAccountButton = findViewById(R.id.CreateAccount_Button);
-
-        // On click, go to "Create Account Activity"
-        NewAccountButton.setOnClickListener(new View.OnClickListener() {
+        // when a user clicks log in, the following logic checks if account is in the database
+        button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateAccount.class ));
+                String email = emailId.getText().toString();
+                String pass = password.getText().toString();
+
+                if (email.isEmpty()) {
+                    // email text was left blank
+                    emailId.setError("Please enter an email address.");
+                    emailId.requestFocus();
+
+                } else if (pass.isEmpty()) {
+                    // password text was left blank
+                    password.setError("Please enter a password.");
+                    password.requestFocus();
+
+                } else if (!email.isEmpty() && !pass.isEmpty()) {
+                    // both values are given, try to create the account
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Could not find account, try again or create a new account.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                                startActivity(i);
+                            }
+                        }
+                    });
+
+                } else {
+                    // This should never happen logically. If it does the world might be imploding
+                    Toast.makeText(MainActivity.this, "Unknown error.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+        // The user wishes create an account
+        button_goto_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CreateAccount.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }

c271b0e1ca18e43f04c06fb1ae5aa5db867ad1c2
Steven Lagrange <slagrang@ualberta.ca>
2019-11-27 14:27:44 -0700

    Added notification accept and decline functionality.

--- app/src/main/java/cmput301/moodi/Objects/MoodiNotificationsAdapter.java
+++ app/src/main/java/cmput301/moodi/Objects/MoodiNotificationsAdapter.java
@@ -1,17 +1,18 @@
 package cmput301.moodi.Objects;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import cmput301.moodi.R;
 
@@ -26,12 +27,14 @@ public class MoodiNotificationsAdapter extends ArrayAdapter<MoodiNotification> {
     private ArrayList<MoodiNotification> notifications;
     private Context context;
     private String TAG = "MoodiNotificationsAdapter";
+    private MoodiStorage moodiStorage;
 
     // Bridge between the list and the displayed list
     public MoodiNotificationsAdapter(Context context, ArrayList<MoodiNotification> notifications){
         super(context, 0, notifications);
         this.notifications = notifications;
         this.context = context;
+        moodiStorage = new MoodiStorage();
     }
 
 
@@ -44,10 +47,12 @@ public class MoodiNotificationsAdapter extends ArrayAdapter<MoodiNotification> {
             view = LayoutInflater.from(context).inflate(R.layout.notification_item, parent,false);
         }
 
-        MoodiNotification notification = notifications.get(position);
+        final MoodiNotification notification = notifications.get(position);
 
         TextView typeView = view.findViewById(R.id.type);
         TextView typeExplanationView = view.findViewById(R.id.type_explanation);
+        Button decline = view.findViewById(R.id.decline_notification);
+        Button accept = view.findViewById(R.id.accept_notification);
 
         String type = notification.getType();
         String sender = notification.getSenderName();
@@ -55,8 +60,27 @@ public class MoodiNotificationsAdapter extends ArrayAdapter<MoodiNotification> {
         typeView.setText(type);
         typeExplanationView.setText(sender);
 
-        Log.d(TAG, "Here now. ");
+       decline.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+                moodiStorage.deleteNotification(notification.getDocumentID());
+           }
+       });
+
+       accept.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               HashMap<String, Object> data = new HashMap<>();
+               data.put("user", notification.getSenderUID());
+               data.put("following", notification.getReceiver());
+               data.put("permission", 2);
+               moodiStorage.createFollower(data);
+               moodiStorage.deleteNotification(notification.getDocumentID());
+           }
+       });
 
         return view;
     }
+
+
 }
